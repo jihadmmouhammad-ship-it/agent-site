acc7a8233095bf5068434ccf3d9b169dfbf0f68d
jihadmmouhammad-ship-it <jihadmmouhammad@gmail.com>
2026-09-05 18:29:06 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
