67d8d4b0a3b206cbd0b50275107752606dc80de3
jihadmmouhammad-ship-it <jihadmmouhammad@gmail.com>
2026-09-13 09:34:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/health', (req, res) => {
